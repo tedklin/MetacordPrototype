0af4329395f6e810b76fe20b4dbf54b48d98dbff
tedklin <tedklin@gmail.com>
2016-04-10 16:58:00 -0700

    Initial Commit

---
+++ MetacordPrototype_1.0Tests/MetacordPrototype_1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MetacordPrototype_1_0Tests.swift
+//  MetacordPrototype_1.0Tests
+//
+//  Created by Ted Lin on 4/10/16.
+//  Copyright © 2016 Ted Lin. All rights reserved.
+//
+
+import XCTest
+@testable import MetacordPrototype_1_0
+
+class MetacordPrototype_1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
